6e96ab16ed07cd91ea71f49c42b3e4a29631d160
stdjes <stephendjes@gmail.com>
2023-09-06 21:44:24 +0000

    task 8 - edges concat fix*

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -2,4 +2,4 @@
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
 print(f"{str[39:66]} {str[107:-18]} {str[:6]}")
-print(str)
+
