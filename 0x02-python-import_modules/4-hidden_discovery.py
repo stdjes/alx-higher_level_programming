034f1b7caa1dc918e2c9aff97097212035e98e64
stdjes <stephendjes@gmail.com>
2023-09-07 11:40:13 +0000

    fix hidden number

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,5 +1,8 @@
+#!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    for name in dir(hidden_4):
-        if name[0] != '_' and name[1] != '_':
+
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
             print(name)
