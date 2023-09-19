c14932c11b7e3b29683c786f9112d0c282d49271
stdjes <stephendjes@gmail.com>
2023-09-19 23:53:43 +0000

    fix negative numbers modulo on last digit game task

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
+# Deal with negative number (because of negative modulo error issues)
+if (number < 0): 
+    last_digit = (-1 * number) % 10 #convert negative number to positive, then find modulo
+    last_digit *= -1 #convert back to negative remainder
 
-last_digit = number % 10
+else:
+    last_digit = number % 10 #for positive numbers
+
 if (last_digit > 5):
     print(f"Last digit of {number} is {last_digit} and is greater than 5")
-
 elif (last_digit == 0):
     print(f"Last digit of {number} is {last_digit} and is 0")
 if (last_digit < 6 and last_digit != 0):
