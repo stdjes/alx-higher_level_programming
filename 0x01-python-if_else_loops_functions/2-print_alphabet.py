7a9a1c469ad4a4f937b6a73b73f2ee1816a8a141
stdjes <stephendjes@gmail.com>
2023-09-20 00:19:48 +0000

    print alphabet with no newline character

---
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -0,0 +1,5 @@
+for char_code in range(ord('a'), ord('z') + 1):
+    print(chr(char_code), end='')
+# alternatively the code can be written as below;
+# for char_code in range(97, 123):
+#    print(chr(char_code), end='')
