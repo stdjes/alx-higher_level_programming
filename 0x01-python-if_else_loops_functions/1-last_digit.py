180124843ff3c06d41b7e78af713b49bb69809dd
stdjes <stephendjes@gmail.com>
2023-09-20 00:05:41 +0000

    fix pycode style for last digit game

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,16 +2,17 @@
 import random
 number = random.randint(-10000, 10000)
 # Deal with negative number (because of negative modulo error issues)
-if (number < 0): 
-    last_digit = (-1 * number) % 10 #convert negative number to positive, then find modulo
-    last_digit *= -1 #convert back to negative remainder
+if (number < 0):
+    # convert negative number to positive, then find modulo
+    l_digit = (-1 * number) % 10
+    l_digit *= -1  # convert back to negative remainder
 
 else:
-    last_digit = number % 10 #for positive numbers
+    l_digit = number % 10  # for positive numbers
 
-if (last_digit > 5):
-    print(f"Last digit of {number} is {last_digit} and is greater than 5")
-elif (last_digit == 0):
-    print(f"Last digit of {number} is {last_digit} and is 0")
-if (last_digit < 6 and last_digit != 0):
-    print(f"Last digit of {number} is {last_digit} and is less than 6 and not 0")
+if (l_digit > 5):
+    print(f"Last digit of {number} is {l_digit} and is greater than 5")
+elif (l_digit == 0):
+    print(f"Last digit of {number} is {l_digit} and is 0")
+if (l_digit < 6 and l_digit != 0):
+    print(f"Last digit of {number} is {l_digit} and is less than 6 and not 0")
